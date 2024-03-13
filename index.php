154cf1ccec9b4b0eae43134490da850a9b2f48d9
Gustavo <gustavo.silva2116@etec.sp.gov.br>
2024-03-13 17:28:51 -0300

    Esta é a primeira aula de PHP

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeira Aula de PHP</title>
+</head>
+<body>
+
+<?php 
+$nome = "Gustavo";
+$idade = 15;
+$altura = 1.73;
+$peso = 63;
+
+var_dump($nome);
+echo "<br>";
+var_dump($idade);
+echo "<br>";
+var_dump($altura);
+echo "<br>";
+var_dump($peso);
+echo "<br>";
+
+echo "Meu nome é $nome";
+echo "<br>";
+echo "Olá Mundo!";
+
+?>    
+</body>
+</html>
